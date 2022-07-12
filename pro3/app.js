7f35e2fe3de5c8574336d2e28c0d8e06754b51c7
jamesjoel <jamesjoelj@gmail.com>
2022-07-12 19:07:47 +0530

    send form to server

---
+++ pro3/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname+"/assets"));
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded());
+
+
+app.get("/", (req, res)=>{
+    res.render("home/index");
+})
+app.get("/about", (req, res)=>{
+    res.render("about/index");
+})
+app.get("/contact", (req, res)=>{
+    res.render("contact/index");
+})
+app.get("/student", (req, res)=>{
+    res.render("student/index");
+})
+
+
+
+app.post("/student/add", (req, res)=>{
+    console.log(req.body);
+    req.body.age = parseInt(req.body.age);
+    console.log(req.body);
+
+})
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log("server running");
+})
