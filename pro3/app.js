5a3736a918b59e9d8b3866013c6d1433f6a1dcb7
jamesjoel <jamesjoelj@gmail.com>
2022-07-14 18:52:12 +0530

    insert in mongodb

--- pro3/app.js
+++ pro3/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
 
 app.use(express.static(__dirname+"/assets"));
 app.set("view engine", "ejs");
@@ -23,12 +24,29 @@ app.get("/student", (req, res)=>{
 
 
 app.post("/student/add", (req, res)=>{
-    console.log(req.body);
     req.body.age = parseInt(req.body.age);
-    console.log(req.body);
+    MongoClient.connect("mongodb://localhost:27017", (err, con)=>{
+        var db = con.db("tss5_30");
+        db.collection("student").insertOne(req.body, (err)=>{
+            // console.log("saved");
+            res.redirect("/student/list");
+        });
+    });
 
 })
 
+
+
+app.get("/student/list", (req, res)=>{
+    MongoClient.connect("mongodb://localhost:27017", (err, con)=>{
+        var db = con.db("tss5_30");
+        db.collection("student").find().toArray((err, result)=>{
+            // console.log(result);
+            var pagedata = { result : result };
+            res.render("student/list", pagedata);
+        });
+    })
+})
 
 
 const port = process.env.PORT || 3000;
