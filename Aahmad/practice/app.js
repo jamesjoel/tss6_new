ce1e1727f12a44923ae5f7fef9c4f2452679d7e7
aahmadburhani <aburhanuddin6@gmail.com>
2022-07-19 04:29:03 -0700

    practice slicing

--- Aahmad/practice/app.js
+++ Aahmad/practice/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
 
 app.use(express.static(__dirname+"/assets"));
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded());
+
 
 
 app.get("/", (req, res)=>{
@@ -19,20 +21,29 @@ app.get("/contact", (req, res)=>{
 app.get("/student", (req, res)=>{
     res.render("student/index");
 })
-app.get("/courses", (req, res)=>{
-    res.render("courses/index");
-})
-
-
-
 app.post("/student/add", (req, res)=>{
-    console.log(req.body);
     req.body.age = parseInt(req.body.age);
-    console.log(req.body);
+    MongoClient.connect("mongodb://localhost:27017", (err, con)=>{
+        var db = con.db("tss5_30");
+        db.collection("student").insertOne(req.body, (err)=>{
+            // console.log("saved");
+            res.redirect("/student/list");
+        });
+    });
 
 })
 
 
+app.get("/student/list", (req, res)=>{
+    MongoClient.connect("mongodb://localhost:27017", (err, con)=>{
+        var db = con.db("tss5_30");
+        db.collection("student").find().toArray((err, result)=>{
+            // console.log(result);
+            var pagedata = { result : result };
+            res.render("student/list", pagedata);
+        });
+    })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
