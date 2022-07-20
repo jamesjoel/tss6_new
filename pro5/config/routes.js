db6ee74c466b5f38f361d78bd9667d6407412e35
jamesjoel <jamesjoelj@gmail.com>
2022-07-20 19:02:27 +0530

    param url

--- pro5/config/routes.js
+++ pro5/config/routes.js
@@ -3,5 +3,6 @@ const routes = require("express").Router();
 
 routes.use("/", require("../controllers/HomeController"));
 routes.use("/teacher", require("../controllers/TeacherController"));
+routes.use("/contact", require("../controllers/ContactController"));
 
 module.exports = routes;
--- pro5/controllers/TeacherController.js
+++ pro5/controllers/TeacherController.js
@@ -1,10 +1,53 @@
 const routes = require("express").Router();
-
+const mongodb = require("mongodb");
+const MongoClient = require("mongodb").MongoClient;
+const dbUrl = "mongodb://localhost:27017";
+const dbName = "tss5_30";
+const collName = "teacher";
 // localhost:3000/teacher
 routes.get("/", (req, res)=>{
-    var pagedata = { title : "Teacher Page", pagename : "teacher/index"};
-    res.render("layout", pagedata);
+    MongoClient.connect(dbUrl, (err, con)=>{
+        var db = con.db(dbName);
+        db.collection(collName).find().toArray((err, result)=>{
+
+            var pagedata = { title : "Teacher Page", pagename : "teacher/index", result : result };
+            res.render("layout", pagedata);
+        })
+    })
 })
 
+routes.post("/add", (req, res)=>{
+    // console.log(req.body);
+    req.body.salary = parseInt(req.body.salary);
+    MongoClient.connect(dbUrl, (err, con)=>{
+        var db = con.db(dbName);
+        db.collection(collName).insertOne(req.body, (err)=>{
+            res.redirect("/teacher");
+        })
+    })
+
+
+})
+
+routes.get("/detail/:a", (req, res)=>{
+    // console.log(req.params.a);
+    var id = mongodb.ObjectId(req.params.a);
+    MongoClient.connect(dbUrl, (err, con)=>{
+        var db = con.db(dbName);
+        db.collection(collName).find({ _id : id }).toArray((err, result)=>{
+            console.log(result);
+            var pagedata = { pagename : "teacher/detail", title : "Detail", result : result[0] };
+            res.render("layout", pagedata);
+        })
+    })
+
+    
+})
+
+// routes.get("/demo/:a/:b/:c", (req, res)=>{
+//     console.log(req.params.a);
+//     console.log(req.params.b);
+//     console.log(req.params.c);
+// })
 
 module.exports = routes;
